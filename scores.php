1564beae394c9c2ae01c9b7381b4eed2f3c50fdd
Jorge Robles <jorgejoanrobles@gmail.com>
2024-01-17 16:55:47 +0100

    manque scores and scoreboard

---
+++ scores.php
@@ -0,0 +1,7 @@
+creer session scores pour l'user 
+
+a chaque response correcte ajouter la valeur des points de la question en la session 
+
+a la fin de la partie faire un insert into tableau scores 
+
+remodifier la variable session score = 0 a la fin de la partie 
